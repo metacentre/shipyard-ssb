9515f70b05e9bcaf57f337cda4264e766adf5235
av8ta <av8ta@protonmail.com>
2021-06-04 15:48:43 +0800

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = [
+  'ssb-private1',
+  'ssb-onion',
+  'ssb-unix-socket',
+  'ssb-no-auth',
+  'ssb-gossip',
+  'ssb-replicate',
+  'ssb-friends',
+  'ssb-blobs',
+  'ssb-invite',
+  'ssb-local',
+  'ssb-logging',
+  'ssb-query',
+  'ssb-links',
+  'ssb-ws',
+  'ssb-ebt',
+  'ssb-ooo'
+]
